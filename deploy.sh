faa3ece77bbe8ee0f1e3a7ce802a90faa6441096
Sam Bleckley <sam@sambleckley.com>
2020-07-27 15:28:24 -0400

    Don't deploy art every time, doofus

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 JEKYLL_ENV="production" bundle exec jekyll build
-aws --profile trajectory s3 cp _site/ s3://sambleckley.com/ --recursive --exclude "Untitled.sketch" --region us-east-2 --cache-control max-age=120 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
+aws --profile trajectory s3 cp _site/ s3://sambleckley.com/ --recursive --exclude "assets/images/art/*" --exclude "Untitled.sketch" --region us-east-2 --cache-control max-age=120 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
